14f70ba6e959685f61215dcab6afe051c3e15b0e
Roman177-png <miron-2002@ukr.net>
2024-11-20 18:35:34 +0100

    feat: implement LinkRepository for data access

--- app/Domain/Link/Repositories/LinkRepository.php
+++ app/Domain/Link/Repositories/LinkRepository.php
@@ -1,7 +1,21 @@
 <?php
 
 namespace App\Domain\Link\Repositories;
+use App\Models\Link;
 
 class LinkRepository
 {
+    public function create(array $data): Link
+    {
+        return Link::create($data);
+    }
+
+    public function findByShortCode(string $shortCode): ?Link
+    {
+        return Link::where('short_code', $shortCode)->first();
+    }
+    public function getAll(): array
+    {
+        return Link::latest()->get()->toArray();
+    }
 }
