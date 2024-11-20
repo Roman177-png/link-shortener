6c7fe4be72b3aacb74dafda6ee388bba0d971b20
Roman177-png <miron-2002@ukr.net>
2024-11-20 18:42:31 +0100

    feat: create LinkService for business logic

---
+++ app/Domain/Link/Services/LinkService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Domain\Link\Services;
+
+use App\Domain\Link\Repositories\LinkRepository;
+use App\Models\Link;
+
+class LinkService
+{
+    private LinkRepository $repository;
+    public function __construct(LinkRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    public function shortenUrl(string $originalUrl): array
+    {
+        $shortCode = $this->generateUniqueShortCode();
+
+        $link = $this->repository->create([
+            'original_url' => $originalUrl,
+            'short_code' => $shortCode,
+        ]);
+        return [
+            'original_url' => $link->original_url,
+            'short_url' => url("/{$link->short_code}"),
+        ];
+    }
+    private function generateUniqueShortCode(): string
+    {
+        do {
+            $shortCode = Link::generateShortCode();
+        } while ($this->repository->findByShortCode($shortCode));
+
+        return $shortCode;
+    }
+}
