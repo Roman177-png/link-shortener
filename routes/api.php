12d747be762020f7e455dce1b442e45c2cb942a3
Roman177-png <miron-2002@ukr.net>
2024-11-20 20:01:25 +0100

    feat: add shorten, get, delete  redirect actions to LinkController

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/shorten', [\App\Http\Controllers\LinkController::class, 'shorten']);
+Route::get('/links', [\App\Http\Controllers\LinkController::class, 'getAllLinks']);
+Route::delete('/{shortCode}', [\App\Http\Controllers\LinkController::class, 'delete']);
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/{shortCode}', [\App\Http\Controllers\LinkController::class, 'redirect']);
